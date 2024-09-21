4b23887b405f0c69dcd995298ad6a6444f48d1ae
Surya Tejess <suryatejessk@gmail.com>
2024-09-21 12:39:41 +0530

    linked_list

---
+++ src/data_structures/linked_list/single/Node.java
@@ -0,0 +1,11 @@
+package data_structures.linked_list.single;
+
+public class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
